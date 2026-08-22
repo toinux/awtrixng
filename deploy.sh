c83e8bede1a5be7c722dec20fe63182f2a2e873b
toinux <toinux@gmail.com>
2026-08-23 01:07:37 +0200

    anothertime -> Anothertime

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/env sh
 ./minify-berry.py anothertime.ax > compact.ax
-curl -H "Content-Type: text/plain" -X PUT --data-binary @compact.ax http://192.168.1.202/api/v1/apps/script/anothertime
+curl -H "Content-Type: text/plain" -X PUT --data-binary @compact.ax http://192.168.1.202/api/v1/apps/script/Anothertime
